e6766b182e8d60cb64b3ea3cefbb6bbee3c5c17a
petemurray45 <pmurray697@gmail.com>
2024-04-11 23:12:45 +0100

    Final version of Caesar Cipher with simplified code.

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
+import art
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-decipher_word = ""
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 
-def encrypt(code_text, code_iteration):
-    cipher_text = ""
-
-    for letter in code_text:
-        position = alphabet.index(letter)
-        new_position = position + code_iteration
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-
-    print(f"The encoded text is {cipher_text}")
-
-
-def decrypt(code_text, code_iteration):
-    deciphered_text = ""
-    for letter in code_text:
-        position = alphabet.index(letter)
-        new_position = position - code_iteration
-        deciphered_text += alphabet[new_position]
-
-    print(f"The decoded text is {deciphered_text}")
+def caesar(start_text, cipher_number, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        cipher_number *= -1
+    for letter in start_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + cipher_number
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
+    print(f"The {cipher_direction}d text is {end_text}")
 
 
-def caesar(a, b, c):
-    if c == "encode":
-        encrypt(a, b)
-    elif c == "decode":
-        decrypt(a, b)
+should_continue = True
 
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
-caesar(text, shift, direction)
+    caesar(text, shift, direction)
 
-
-
+    result = input("Type 'yes' if you would like to go again, otherwise type 'no\n ").lower()
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
 
