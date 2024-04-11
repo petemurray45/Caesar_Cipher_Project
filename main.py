2580c68751167d3909494a4c1d7117e30a043132
petemurray45 <pmurray697@gmail.com>
2024-04-11 16:13:16 +0100

    reorganised code to only execute one function, using the other two inside

--- main.py
+++ main.py
@@ -8,27 +8,37 @@ shift = int(input("Type the shift number:\n"))
 decipher_word = ""
 
 
-def encrypt(phrase, iteration):
+def encrypt(code_text, code_iteration):
     cipher_text = ""
 
-    for letter in phrase:
+    for letter in code_text:
         position = alphabet.index(letter)
-        new_position = position + iteration
+        new_position = position + code_iteration
         new_letter = alphabet[new_position]
         cipher_text += new_letter
+
     print(f"The encoded text is {cipher_text}")
 
 
-def decrypt(cipher_text, new_iteration):
+def decrypt(code_text, code_iteration):
     deciphered_text = ""
-    for letter in cipher_text:
+    for letter in code_text:
         position = alphabet.index(letter)
-        new_position = position - new_iteration
+        new_position = position - code_iteration
         deciphered_text += alphabet[new_position]
+
     print(f"The decoded text is {deciphered_text}")
 
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+def caesar(a, b, c):
+    if c == "encode":
+        encrypt(a, b)
+    elif c == "decode":
+        decrypt(a, b)
+
+
+caesar(text, shift, direction)
+
+
+
+
